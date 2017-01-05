abed8540523e3bc957b338a8f9ef60d6fdea3d4f
Sylvain Boulade <bopulie@gmail.com>
2017-01-05 09:21:32 +0100

    Add remaining Grid component tests

--- js/components/Grid.test.js
+++ js/components/Grid.test.js
@@ -4,14 +4,20 @@ import { shallowToJson } from 'enzyme-to-json';
 import { Plain } from './Grid';
 import Cell from './Cell';
 
-function makeTestGrid(gridSize) {
+function makeTestGrid(gridSize, gameStatus, cellsFlagged, cellsOpened) {
   const size = gridSize || 20;
+  const status = gameStatus || 'running';
   const testGridProp = {
     width: 1,
     height: 1,
-    cells: new Array(size).fill(undefined).map((cell, i) => ({ index: i }))
+    cells: new Array(size).fill(undefined).map((cell, i) => ({
+      index: i,
+      flagged: cellsFlagged ? true : false,
+      opened: cellsOpened ? true : false,
+      value: 2
+    }))
   };
-  return shallow(<Plain grid={testGridProp} />);
+  return shallow(<Plain grid={testGridProp} status={status} />);
 }
 
 test('Snapshot', () => {
@@ -24,4 +30,50 @@ test('Should render a number of cells based off grid.cells prop', () => {
   const gridSize = 80;
   const component = makeTestGrid(gridSize);
   expect(component.find(Cell).length).toEqual(gridSize);
+});
+
+test('Should pass cell props correctly to child Cell component', () => {
+  const gridSize = 1;
+  const component = makeTestGrid(gridSize);
+  const child = component.find(Cell);
+  const childProps = child.props();
+  expect(child.length).toEqual(gridSize);
+  expect(childProps.index).toEqual(0);
+  expect(childProps.flagged).toEqual(false);
+  expect(childProps.opened).toEqual(false);
+  expect(childProps.value).toEqual(2);
+  expect(childProps.handleLeftClick).toBeDefined();
+  expect(childProps.handleRightClick).toBeDefined();
+});
+
+test('Should NOT pass handleLeftClick/handleRightClick to an opened cell', () => {
+  const component = makeTestGrid(1, 'running', false, true);
+  const child = component.find(Cell);
+  const childProps = child.props();
+  expect(childProps.handleLeftClick).toBeUndefined();
+  expect(childProps.handleRightClick).toBeUndefined();
+});
+
+test('Should NOT pass handleLeftClick to a flagged cell, but should pass handleRightClick', () => {
+  const component = makeTestGrid(1, 'running', true, false);
+  const child = component.find(Cell);
+  const childProps = child.props();
+  expect(childProps.handleLeftClick).toBeUndefined();
+  expect(childProps.handleRightClick).toBeDefined();
+});
+
+test('Should NOT pass handleLeftClick/handleRightClick if the game is won', () => {
+  const component = makeTestGrid(1, 'won', false, false);
+  const child = component.find(Cell);
+  const childProps = child.props();
+  expect(childProps.handleLeftClick).toBeUndefined();
+  expect(childProps.handleRightClick).toBeUndefined();
+});
+
+test('Should NOT pass handleLeftClick/handleRightClick if the game is lost', () => {
+  const component = makeTestGrid(1, 'lost', false, false);
+  const child = component.find(Cell);
+  const childProps = child.props();
+  expect(childProps.handleLeftClick).toBeUndefined();
+  expect(childProps.handleRightClick).toBeUndefined();
 });
